e213dc5193bb5604993b642f8bbd0f7b9690c5fa
pratikab <pratikab@iitk.ac.in>
2017-02-09 21:42:53 +0530

    Complete LEX

--- src/lex.py
+++ src/lex.py
@@ -1,59 +1,67 @@
 
 import pydot
 
-tokens = (
-    # RESERVED
-    'AUTO', 'BREAK', 'CASE', 'CHAR', 'CONST', 'CONTINUE', 'DEFAULT', 'DO', 'DOUBLE',
-    'ELSE', 'ENUM', 'EXTERN', 'FLOAT', 'FOR', 'GOTO', 'IF', 'INT', 'LONG', 'REGISTER',
-    'RETURN', 'SHORT', 'SIGNED', 'SIZEOF', 'STATIC', 'STRUCT', 'SWITCH', 'TYPEDEF',
-    'UNION', 'UNSIGNED', 'VOID', 'VOLATILE', 'WHILE',
+reserved = {
+    'auto' : 'AUTO',
+    'break' : 'BREAK',
+    'case' : 'CASE',
+    'char' : 'CHAR',
+    'const' : 'CONST',
+    'continue' : 'CONTINUE',
+    'default' : 'DEFAULT',
+    'do' : 'DO',
+    'double' : 'DOUBLE',    
+    'else' : 'ELSE',
+    'enum' : 'ENUM',
+    'extern' : 'EXTERN',
+    'float' : 'FLOAT',
+    'for' : 'FOR',
+    'goto' : 'GOTO',
+    'if' : 'IF',
+    'int' : 'INT',
+    'long' : 'LONG',
+    'register' : 'REGISTER',
+    'return' : 'RETURN',
+    'short' : 'SHORT',
+    'signed' : 'SIGNED',
+    'sizeof' : 'SIZEOF',
+    'static' : 'STATIC',
+    'struct' : 'STRUCT',
+    'switch' : 'SWITCH',
+    'typedef' : 'TYPEDEF',
+    'union' : 'UNION',
+    'unsigned' : 'UNSIGNED',
+    'void' : 'VOID',
+    'volatile' : 'VOLATILE',
+    'while' : 'WHILE',
+}
 
-    # OPERATORS 
+
+tokens = list(reserved.values()) + [
+    # RESERVED
+    #'AUTO', 'BREAK', 'CASE', 'CHAR', 'CONST', 'CONTINUE', 'DEFAULT', 'DO', 'DOUBLE',
+    #'ELSE', 'ENUM', 'EXTERN', 'FLOAT', 'FOR', 'GOTO', 'IF', 'INT', 'LONG', 'REGISTER',
+    #'RETURN', 'SHORT', 'SIGNED', 'SIZEOF', 'STATIC', 'STRUCT', 'SWITCH', 'TYPEDEF',
+    #'UNION', 'UNSIGNED', 'VOID', 'VOLATILE', 'WHILE',
+
+    #OPERATORS 
     'ELLIPSIS','RIGHT_ASSIGN','LEFT_ASSIGN','ADD_ASIGN','SUB_ASSIGN','MUL_ASSIGN','DIV_ASSIGN',
     'MOD_ASSIGN','AND_ASSIGN','XOR_ASSIGN','OR_ASSIGN','RIGHT_OP','LEFT_OP','INC_OP','DEC_OP',
     'PTR_OP','AND_OP','OP_OP','LE_OP','GE_OP','EQ_OP','NE_OP','SEMICOLON','OPEN_CURLY','CLOSE_CURLY',
     'COMMA','COLON','EQUAL','OPEN_PAR','CLOSE_PAR','OPEN_SQUARE','CLOSE_SQUARE','DOT','AND','EXCLAIM',
     'NOT','MINUS','PLUS','MUL','DIVIDE','MOD','LESS_THAN','GREATER_THAN','XOR','OR','COND_OP',
-
-    # LITERALS
-    'ID','INT_CONST','FLOAT_CONST','STRING_CONSTANT','CHAR_CONST',
-)
+    #LITERALS
+    'ID','SCONST','CONSTANT',
+]
 
 
 # Tokens
-
-t_AUTO = r'AUTO'
-t_BREAK = r'BREAK'
-t_CASE = r'CASE'
-t_CHAR = r'CHAR'
-t_CONST = r'CONST'
-t_CONTINUE = r'CONTINUE'
-t_DEFAULT = r'DEFAULT'
-t_DO = r'DO'
-t_DOUBLE   = r'DOUBLE'    
-t_ELSE = r'ELSE'
-t_ENUM = r'ENUM'
-t_EXTERN = r'EXTERN'
-t_FLOAT = r'FLOAT'
-t_FOR = r'FOR'
-t_GOTO = r'GOTO'
-t_IF = r'IF'
-t_INT = r'INT'
-t_LONG = r'LONG'
-t_REGISTER = r'REGISTER'
-t_RETURN = r'RETURN'
-t_SHORT = r'SHORT'
-t_SIGNED = r'SIGNED'
-t_SIZEOF = r'SIZEOF'
-t_STATIC = r'STATIC'
-t_STRUCT = r'STRUCT'
-t_SWITCH = r'SWITCH'
-t_TYPEDEF = r'TYPEDEF'
-t_UNION = r'UNION'
-t_UNSIGNED = r'UNSIGNED'
-t_VOID = r'VOID'
-t_VOLATILE = r'VOLATILE'
-t_WHILE = r'WHILE'
+D = r'[0-9]'
+L = r'[a-zA-Z_]'
+H = r'[a-fA-F0-9]'
+E = r'[Ee][+-]?{D}+'
+FS = r'(f|F|l|L)'
+IS = r'(u|U|l|L)*'
 
 t_ELLIPSIS = r'\.\.\.'
 t_RIGHT_ASSIGN = r'>>='
@@ -104,20 +112,20 @@ t_COND_OP = r'\?'
 
 t_ignore = " \t\v\n\f"
 
-
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
+def t_SCONST(t):
+    r'L?\"(\\.|[^\\"])*\"'
 
-
+def t_CONSTANT(t):
+    r'(0[xX]{H}+{IS}?)|(0{D}+{IS}?)|({D}+{IS}?)|({D}+{E}{FS}?)|({D}*"."{D}+({E})?{FS}?)|({D}+"."{D}*({E})?{FS}?)'
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
