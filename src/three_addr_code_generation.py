c1b3e6ae78fc41532bee89390f241b215add868a
Shibhansh Dohare <shibhanshdohare1997@gmail.com>
2017-04-19 23:51:23 +0530

    add/icg class

--- src/three_addr_code_generation.py
+++ src/three_addr_code_generation.py
@@ -2,4 +2,11 @@ import sys
 import cparser
 
 temp, symbol_table = cparser.main()
-print temp, symbol_table
+
+
+class icg(object):
+  def __init__(self, code='',place=0):
+  	self.code = code
+  	self.place = 0
+  def gen():
+  	pass
